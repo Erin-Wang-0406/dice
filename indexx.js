fe64f29ed766d2cd9e23e393a84f0a18de77056e
Lil Neidog <63150806+Erin-Wang-0406@users.noreply.github.com>
2023-10-24 09:26:40 +0800

    Add files via upload

---
+++ indexx.js
@@ -0,0 +1,11 @@
+var random1 = Math.floor(Math.random() * 6) + 1;
+document.querySelectorAll("img")[0].setAttribute("src", "./images/dice" + random1 + ".png");
+var random2 = Math.floor(Math.random() * 6) + 1;
+document.querySelectorAll("img")[1].setAttribute("src", "./images/dice" + random2 + ".png");
+if (random1 === random2) {
+    document.querySelector("h1").innerHTML = "Tie!";
+} else if (random1 > random2) {
+    document.querySelector("h1").innerHTML = "🚩Player1 Win!";
+} else if (random1 < random2) {
+    document.querySelector("h1").innerHTML = "Player2 Win!🚩";
+}
